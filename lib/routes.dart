36c2555514447e5af1f6e7f3bc94f03ccbbe6a4f
MohmedFahmy <mohammedfahmy130@gmail.com>
2026-01-08 13:57:52 +0200

    feat: Add mobile and web blog pages; implement basic structure and routing

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/mobile/about_mobile.dart';
+import 'package:portfolio/mobile/blog_mobile.dart';
 import 'package:portfolio/mobile/contact_mobile.dart';
 import 'package:portfolio/mobile/landing_mobile_page.dart';
 import 'package:portfolio/web/about_web.dart';
+import 'package:portfolio/web/blog_web.dart';
 import 'package:portfolio/web/contact_web.dart';
 import 'package:portfolio/web/landing_web_page.dart';
 
@@ -27,9 +29,9 @@ class Routes {
           builder: (_) => LayoutBuilder(
             builder: (context, constraints) {
               if (constraints.maxWidth > 800) {
-                return  ContactWeb();
+                return ContactWeb();
               } else {
-                return  ContactMobile();
+                return ContactMobile();
               }
             },
           ),
@@ -40,9 +42,22 @@ class Routes {
           builder: (_) => LayoutBuilder(
             builder: (context, constraints) {
               if (constraints.maxWidth > 800) {
-                return  AboutWeb();
+                return AboutWeb();
               } else {
-                return  AboutMobile();
+                return AboutMobile();
+              }
+            },
+          ),
+          settings: settings,
+        );
+      case '/blog':
+        return MaterialPageRoute(
+          builder: (_) => LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 800) {
+                return BlogWeb();
+              } else {
+                return BlogMobile();
               }
             },
           ),
